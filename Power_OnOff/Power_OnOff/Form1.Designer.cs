ae01ed2886d57d80f2834a9059c6a787e88d6221
Parker <b511015@gmail.com>
2015-02-09 18:38:12 +0800

    first time.

---
+++ Power_OnOff/Power_OnOff/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblGetTime = new System.Windows.Forms.Label();
+            this.GroupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblOFFcounter = new System.Windows.Forms.Label();
+            this.lblRUNcounter = new System.Windows.Forms.Label();
+            this.cmdClean = new System.Windows.Forms.Button();
+            this.Label8 = new System.Windows.Forms.Label();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.txtOff_time = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.cmdStop = new System.Windows.Forms.Button();
+            this.cmdStart = new System.Windows.Forms.Button();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.txtRun_time = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmdConnect = new System.Windows.Forms.Button();
+            this.cmbportname = new System.Windows.Forms.ComboBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.GetTimer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.UItmr = new System.Windows.Forms.Timer(this.components);
+            this.GroupBox3.SuspendLayout();
+            this.GroupBox2.SuspendLayout();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblGetTime
+            // 
+            this.lblGetTime.AutoSize = true;
+            this.lblGetTime.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblGetTime.Location = new System.Drawing.Point(295, 28);
+            this.lblGetTime.Name = "lblGetTime";
+            this.lblGetTime.Size = new System.Drawing.Size(45, 12);
+            this.lblGetTime.TabIndex = 7;
+            this.lblGetTime.Text = "GetTime";
+            // 
+            // GroupBox3
+            // 
+            this.GroupBox3.Controls.Add(this.lblOFFcounter);
+            this.GroupBox3.Controls.Add(this.lblRUNcounter);
+            this.GroupBox3.Controls.Add(this.cmdClean);
+            this.GroupBox3.Controls.Add(this.Label8);
+            this.GroupBox3.Controls.Add(this.Label7);
+            this.GroupBox3.Location = new System.Drawing.Point(12, 188);
+            this.GroupBox3.Name = "GroupBox3";
+            this.GroupBox3.Size = new System.Drawing.Size(388, 70);
+            this.GroupBox3.TabIndex = 6;
+            this.GroupBox3.TabStop = false;
+            this.GroupBox3.Text = "Counter";
+            // 
+            // lblOFFcounter
+            // 
+            this.lblOFFcounter.AutoSize = true;
+            this.lblOFFcounter.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblOFFcounter.Location = new System.Drawing.Point(117, 47);
+            this.lblOFFcounter.Name = "lblOFFcounter";
+            this.lblOFFcounter.Size = new System.Drawing.Size(11, 12);
+            this.lblOFFcounter.TabIndex = 10;
+            this.lblOFFcounter.Text = "0";
+            // 
+            // lblRUNcounter
+            // 
+            this.lblRUNcounter.AutoSize = true;
+            this.lblRUNcounter.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblRUNcounter.Location = new System.Drawing.Point(117, 21);
+            this.lblRUNcounter.Name = "lblRUNcounter";
+            this.lblRUNcounter.Size = new System.Drawing.Size(11, 12);
+            this.lblRUNcounter.TabIndex = 9;
+            this.lblRUNcounter.Text = "0";
+            // 
+            // cmdClean
+            // 
+            this.cmdClean.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdClean.Location = new System.Drawing.Point(305, 21);
+            this.cmdClean.Name = "cmdClean";
+            this.cmdClean.Size = new System.Drawing.Size(75, 25);
+            this.cmdClean.TabIndex = 8;
+            this.cmdClean.Text = "Clean";
+            this.cmdClean.UseVisualStyleBackColor = true;
+            this.cmdClean.Click += new System.EventHandler(this.cmdClean_Click);
+            // 
+            // Label8
+            // 
+            this.Label8.AutoSize = true;
+            this.Label8.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label8.Location = new System.Drawing.Point(11, 47);
+            this.Label8.Name = "Label8";
+            this.Label8.Size = new System.Drawing.Size(100, 12);
+            this.Label8.TabIndex = 1;
+            this.Label8.Text = "Power Off Counter :";
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label7.Location = new System.Drawing.Point(11, 21);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(98, 12);
+            this.Label7.TabIndex = 0;
+            this.Label7.Text = "Power On Counter :";
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.Label5);
+            this.GroupBox2.Controls.Add(this.txtOff_time);
+            this.GroupBox2.Controls.Add(this.Label3);
+            this.GroupBox2.Controls.Add(this.cmdStop);
+            this.GroupBox2.Controls.Add(this.cmdStart);
+            this.GroupBox2.Controls.Add(this.Label4);
+            this.GroupBox2.Controls.Add(this.txtRun_time);
+            this.GroupBox2.Controls.Add(this.Label2);
+            this.GroupBox2.Location = new System.Drawing.Point(12, 99);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(388, 83);
+            this.GroupBox2.TabIndex = 4;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "Run/Off Interval";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(148, 24);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(132, 12);
+            this.Label5.TabIndex = 12;
+            this.Label5.Text = "second, and then Power On";
+            // 
+            // txtOff_time
+            // 
+            this.txtOff_time.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOff_time.Location = new System.Drawing.Point(75, 21);
+            this.txtOff_time.MaxLength = 10;
+            this.txtOff_time.Name = "txtOff_time";
+            this.txtOff_time.Size = new System.Drawing.Size(67, 22);
+            this.txtOff_time.TabIndex = 11;
+            this.txtOff_time.Text = "1";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label3.Location = new System.Drawing.Point(15, 24);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(54, 12);
+            this.Label3.TabIndex = 10;
+            this.Label3.Text = "Off Time :";
+            // 
+            // cmdStop
+            // 
+            this.cmdStop.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdStop.Location = new System.Drawing.Point(305, 49);
+            this.cmdStop.Name = "cmdStop";
+            this.cmdStop.Size = new System.Drawing.Size(75, 25);
+            this.cmdStop.TabIndex = 7;
+            this.cmdStop.Text = "Stop";
+            this.cmdStop.UseVisualStyleBackColor = true;
+            this.cmdStop.Click += new System.EventHandler(this.cmdStop_Click);
+            // 
+            // cmdStart
+            // 
+            this.cmdStart.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdStart.Location = new System.Drawing.Point(305, 18);
+            this.cmdStart.Name = "cmdStart";
+            this.cmdStart.Size = new System.Drawing.Size(75, 25);
+            this.cmdStart.TabIndex = 6;
+            this.cmdStart.Text = "Start";
+            this.cmdStart.UseVisualStyleBackColor = true;
+            this.cmdStart.Click += new System.EventHandler(this.cmdStart_Click);
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(146, 55);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(134, 12);
+            this.Label4.TabIndex = 4;
+            this.Label4.Text = "second, and then Power Off";
+            // 
+            // txtRun_time
+            // 
+            this.txtRun_time.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRun_time.Location = new System.Drawing.Point(75, 52);
+            this.txtRun_time.MaxLength = 10;
+            this.txtRun_time.Name = "txtRun_time";
+            this.txtRun_time.Size = new System.Drawing.Size(67, 22);
+            this.txtRun_time.TabIndex = 2;
+            this.txtRun_time.Text = "1";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label2.Location = new System.Drawing.Point(11, 55);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(58, 12);
+            this.Label2.TabIndex = 0;
+            this.Label2.Text = "Run Time :";
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.cmdConnect);
+            this.GroupBox1.Controls.Add(this.cmbportname);
+            this.GroupBox1.Controls.Add(this.Label1);
+            this.GroupBox1.Location = new System.Drawing.Point(12, 43);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(388, 50);
+            this.GroupBox1.TabIndex = 5;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "PC Com Port";
+            // 
+            // cmdConnect
+            // 
+            this.cmdConnect.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdConnect.Location = new System.Drawing.Point(273, 15);
+            this.cmdConnect.Name = "cmdConnect";
+            this.cmdConnect.Size = new System.Drawing.Size(107, 25);
+            this.cmdConnect.TabIndex = 1;
+            this.cmdConnect.Text = "Connect";
+            this.cmdConnect.UseVisualStyleBackColor = true;
+            this.cmdConnect.Click += new System.EventHandler(this.cmdConnect_Click);
+            // 
+            // cmbportname
+            // 
+            this.cmbportname.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbportname.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cmbportname.FormattingEnabled = true;
+            this.cmbportname.Location = new System.Drawing.Point(88, 18);
+            this.cmbportname.Name = "cmbportname";
+            this.cmbportname.Size = new System.Drawing.Size(116, 20);
+            this.cmbportname.TabIndex = 1;
+            this.cmbportname.SelectedIndexChanged += new System.EventHandler(this.cmbportname_SelectedIndexChanged);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Label1.Location = new System.Drawing.Point(11, 21);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(71, 12);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Port Number :";
+            // 
+            // GetTimer
+            // 
+            this.GetTimer.Tick += new System.EventHandler(this.GetTimer_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(411, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // UItmr
+            // 
+            this.UItmr.Enabled = true;
+            this.UItmr.Tick += new System.EventHandler(this.UItmr_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 278);
+            this.Controls.Add(this.lblGetTime);
+            this.Controls.Add(this.GroupBox3);
+            this.Controls.Add(this.GroupBox2);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Power On/Off Control_v2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.GroupBox3.ResumeLayout(false);
+            this.GroupBox3.PerformLayout();
+            this.GroupBox2.ResumeLayout(false);
+            this.GroupBox2.PerformLayout();
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblGetTime;
+        internal System.Windows.Forms.GroupBox GroupBox3;
+        internal System.Windows.Forms.Label lblOFFcounter;
+        internal System.Windows.Forms.Label lblRUNcounter;
+        internal System.Windows.Forms.Button cmdClean;
+        internal System.Windows.Forms.Label Label8;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        internal System.Windows.Forms.Button cmdStop;
+        internal System.Windows.Forms.Button cmdStart;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.TextBox txtRun_time;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.Label Label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer GetTimer;
+        public System.Windows.Forms.ComboBox cmbportname;
+        internal System.Windows.Forms.Button cmdConnect;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.TextBox txtOff_time;
+        internal System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Timer UItmr;
+
+    }
+}
+
---
+++ Power_OnOff/Power_OnOff/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string appPATH = Application.StartupPath;    //C#取得主程式路徑(Application Path)
+        DateTime ProgramStartTime = DateTime.Now;
+
+        Thread workThread;
+
+        // API: timeGetTime
+        [DllImport("winmm")]
+        private static extern uint timeGetTime();
+
+        double Savetime = 0;
+        double offtimeInterval, runtimeInterval;
+        long offCounter, runCounter;
+        bool RUNNING;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //獲取計算機有效串口
+                string[] ports = SerialPort.GetPortNames();
+                foreach (string port in ports)
+                {
+                    cmbportname.Items.Add(port);
+                }
+                cmbportname.Sorted = true; ; //排序
+                cmbportname.SelectedIndex = 0;
+
+                GetTimer.Start();
+                RUNNING = false;
+            }
+            catch (Exception ex)
+            {
+                //例外處理
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show(this, "確定退出 ？", "退出視窗通知", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                // Cancel 取得或設定數值，表示是否應該取消事件。
+                e.Cancel = false;
+            }
+            else { e.Cancel = true; }
+        }
+
+        private void GetTimer_Tick(object sender, EventArgs e)
+        {
+            lblGetTime.Text = string.Format(DateTime.Now.ToString("yyyy-MM-dd")) + " " + string.Format(DateTime.Now.ToString("HH:mm:ss"));
+            //lblGetTime.Text = string.Format(DateTime.Now.ToString("yyyy-MM-dd")) + " " + string.Format(DateTime.Now.ToString("HH:mm:ss"));
+        }
+
+        private void cmdConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Open();
+                cmdConnect.Text = "Disconnect";
+            }
+            catch (Exception)
+            {
+                serialPort1.Close();
+                cmdConnect.Text = "Connect";
+                //MessageBox.Show(ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void cmbportname_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cmbportname.Text;
+                serialPort1.BaudRate = 115200;
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.DataBits = 8;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            finally
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void cmdStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    runtimeInterval = double.Parse(txtRun_time.Text) * 1000;
+                    offtimeInterval = double.Parse(txtOff_time.Text) * 1000;
+                    RUNNING = true;
+                    if (workThread == null || !workThread.IsAlive)
+                    {
+                        ThreadStart backgroundThread = new ThreadStart(backgroundWork);
+                        workThread = new Thread(backgroundThread);
+                        workThread.IsBackground = true;
+                        workThread.Start();
+                        log_addtime(appPATH, "Start!" + " " + "Set run time:" + txtRun_time.Text + "s" + " and off time:" + txtOff_time.Text + "s");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Com port is disconnected !", "Com port", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace, "Com port", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void cmdStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (workThread != null && workThread.IsAlive)
+                {
+                    workThread.Abort();
+                    RUNNING = false;
+                    log_addtime(appPATH, "Stop!" + " " + "Power on counter=" + runCounter.ToString() + " and Power off counter=" + offCounter.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void cmdClean_Click(object sender, EventArgs e)
+        {
+            log_addtime(appPATH, "Clean Counter!" + " " + "Power on counter=" + runCounter.ToString() + " and Power off counter=" + offCounter.ToString());
+            offCounter = 0;
+            runCounter = 0;
+        }
+
+        private void backgroundWork()
+        {
+            try
+            {
+                while (true)
+                {
+                    // off time
+                    Savetime = timeGetTime(); //記下開始時的時間
+                    while (timeGetTime() < Savetime + offtimeInterval) //循環等待
+                    {
+                        Application.DoEvents();
+                        System.Threading.Thread.Sleep(1);
+                    }
+                    // then power on
+                    log_addtime(appPATH, "power on");
+                    serialPort1.Write("atop_do 1 1\r\n");
+                    runCounter++;
+                    // run time
+                    Savetime = timeGetTime(); //記下開始時的時間
+                    while (timeGetTime() < Savetime + runtimeInterval) //循環等待
+                    {
+                        Application.DoEvents();
+                        System.Threading.Thread.Sleep(1);
+                    }
+                    // then power off
+                    log_addtime(appPATH, "power off");
+                    serialPort1.Write("atop_do 1 0\r\n");
+                    offCounter++;
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace, "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                RUNNING = false;
+                //log_addtime(appPATH, ex.Message + ex.StackTrace);
+            }
+        }
+
+        private delegate void myUICallBack(string myStr, Control ctl);
+
+        private void counterUI(string myStr, Control ctl)
+        {
+            if (this.InvokeRequired)
+            {
+                myUICallBack myUpdate = new myUICallBack(counterUI);
+                this.Invoke(myUpdate, myStr, ctl);
+            }
+            else
+            {
+                ctl.Text = myStr;
+            }
+        }
+
+        private void UItmr_Tick(object sender, EventArgs e)
+        {
+            lblOFFcounter.Text = offCounter.ToString();
+            lblRUNcounter.Text = runCounter.ToString();
+
+            if (RUNNING)
+            {
+                cmbportname.Enabled = false;
+                cmdConnect.Enabled = false;
+                txtOff_time.Enabled = false;
+                txtRun_time.Enabled = false;
+                cmdStart.Enabled = false;
+                cmdStop.Enabled = true;
+            }
+            else
+            {
+                cmbportname.Enabled = true;
+                cmdConnect.Enabled = true;
+                txtOff_time.Enabled = true;
+                txtRun_time.Enabled = true;
+                cmdStart.Enabled = true;
+                cmdStop.Enabled = true;
+            }
+        }
+
+        #region 寫入csv(txt)Log
+
+        private void log_addtime(string SaveFileName, string tmpstr)
+        {
+            DateTime time = DateTime.Now;
+            string tmpLogstr;
+            // '\r'是回車，'\n'是換行，前者使游標到行首，後者使游標下移一格。通常用的Enter是兩個加起來。
+            tmpLogstr = String.Format("{0:00}:{1:00}:{2:00}", time.Hour, time.Minute, time.Second) + "   " + tmpstr + "\r";
+            //tmpLogstr = String.Format("{0:00}/{1:00}/{2:00},{3:00}:{4:00}:{5:00}", time.Year, time.Month, time.Day, time.Hour, time.Minute, time.Second) + "," + tmpstr + "\r";
+            writeLog_csv(SaveFileName, tmpLogstr);
+        }
+
+        private void writeLog_csv(string SaveFilePath, string SaveLogstr)
+        {
+            // 建立檔案
+            FileStream fs = File.Open(SaveFilePath + @"\" + String.Format("{0:00}-{1:00}-{2:00}", ProgramStartTime.Year, ProgramStartTime.Month, ProgramStartTime.Day) + ".txt", FileMode.Append, FileAccess.Write);
+            // 建構StreamWriter物件
+            StreamWriter sw = new StreamWriter(fs);
+            try
+            {
+                // 寫入
+                sw.WriteLine(SaveLogstr);
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+                //例外處理
+                MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace);
+            }
+            finally
+            {
+                // 清除目前寫入器(Writer)的所有緩衝區，並且造成任何緩衝資料都寫入基礎資料流
+                sw.Flush();
+
+                // 關閉目前的StreamWriter物件和基礎資料流
+                sw.Close();
+                fs.Close();
+            }
+        }
+
+        #endregion 寫入csv(txt)Log
+    }
+}
